26f6a9605847293d579333472e5f16ae5c19c3e9
“Aliaksandr <aliaksandr.yanushkevich@gmail.com>
2019-10-13 20:28:49 +0300

    feat: solve 100/100

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.min + this.max) / 2); // divide input range on two pieces
     }
 
     lower() {
-
+        this.max =  Math.round((this.min + this.max) / 2); // if guessing number in first piece of the range
     }
 
     greater() {
-
+        this.min =  Math.round((this.min + this.max) / 2); // if guessing number in second piece of the range
     }
 }
 
